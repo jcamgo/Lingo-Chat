5974958c631d8370c852893c5b152320f93fbebc
jonny <jonnycamargo96@gmail.com>
2019-01-28 22:39:41 -0700

    Initial Commit

---
+++ Lingo Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lingo Chat
+//
+//  Created by Jonathan Orellana on 1/28/19.
+//  Copyright © 2019 jonathan orellana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
