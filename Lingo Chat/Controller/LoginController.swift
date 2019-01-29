d9334d475aca2865f5aadfe13ee61200c7f70346
jonny <jonnycamargo96@gmail.com>
2019-01-28 23:50:51 -0700

    Creating Login screen

---
+++ Lingo Chat/Controller/LoginController.swift
@@ -0,0 +1,41 @@
+//
+//  LoginController.swift
+//  Lingo Chat
+//
+//  Created by Jonathan Orellana on 1/28/19.
+//  Copyright © 2019 jonathan orellana. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = UIColor(r: 61, g: 91, b: 151)
+        
+        let inputsContainerView = UIView()
+        inputsContainerView.backgroundColor = UIColor.white
+        inputsContainerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(inputsContainerView)
+        
+        // need x, y, width, height constraints
+        inputsContainerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        inputsContainerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        inputsContainerView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+        inputsContainerView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+}
+
+extension UIColor {
+    
+    convenience init(r: CGFloat, g: CGFloat, b: CGFloat) {
+        self.init(red: r/255, green: g/255, blue: b/255, alpha: 1)
+    }
+}
---
+++ Lingo Chat/Controller/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Lingo Chat
+//
+//  Created by Jonathan Orellana on 1/28/19.
+//  Copyright © 2019 jonathan orellana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handleLogout))
+    }
+    
+    
+    @objc func handleLogout() {
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+    }
+
+}
+
