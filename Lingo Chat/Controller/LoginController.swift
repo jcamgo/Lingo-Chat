f1f83a793fc76f4520c87259e1405b481c0c6809
jonny <jonnycamargo96@gmail.com>
2019-02-15 21:54:45 -0700

    Fix Brackets

--- Lingo Chat/Controller/LoginController.swift
+++ Lingo Chat/Controller/LoginController.swift
@@ -60,9 +60,7 @@ class LoginController: UIViewController {
                     print(err)
                     return
                 }
-                
                 print("Saved user successfully into Firebase db")
-                
             })
         })
     }
